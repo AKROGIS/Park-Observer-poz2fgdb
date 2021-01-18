f1e5fe97d461bbdc430a0f30eb5ed4a4792e43a2
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-01-17 18:03:17 -0900

    lint MakeService.py

--- MakeService.py
+++ MakeService.py
@@ -11,41 +11,49 @@ Requires the Esri ArcGIS arcpy module.
 
 from __future__ import print_function
 
-import arcpy
 import os
 
+import arcpy
+
 # define local variables
-workspace = r"D:\MapData\Observer"
-protocol = "Test_Protocol_v1"
-mxd = os.path.join(workspace, protocol + ".mxd")
-mapDoc = arcpy.mapping.MapDocument(mxd)
-# connectionfile = 'GIS Servers/arcgis on MyServer_6080 (publisher).ags'
+WORKSPACE = r"D:\MapData\Observer"
+PROTOCOL = "Test_Protocol_v1"
 
-sddraft = os.path.join(workspace, protocol + ".sddraft")
-sd = os.path.join(workspace, protocol + ".sd")
+def main():
+    """Create and upload service definition"""
 
-summary = "Survey Protocol " + protocol
-tags = "Survey, Protocol, Park Observer, " + protocol
+    mxd = os.path.join(WORKSPACE, PROTOCOL + ".mxd")
+    map_doc = arcpy.mapping.MapDocument(mxd)
+    # connectionfile = 'GIS Servers/arcgis on MyServer_6080 (publisher).ags'
 
-# create service definition draft
-analysis = arcpy.mapping.CreateMapSDDraft(
-    mapDoc,
-    sddraft,
-    protocol,
-    "ARCGIS_SERVER",
-    None,
-    False,
-    "ParkObserver",
-    summary,
-    tags,
-)
+    sddraft = os.path.join(WORKSPACE, PROTOCOL + ".sddraft")
+    sd_file = os.path.join(WORKSPACE, PROTOCOL + ".sd")
 
-# stage and upload the service if the sddraft analysis did not contain errors
-if analysis["errors"] == {}:
-    # Execute StageService
-    arcpy.StageService_server(sddraft, sd)
-    # Execute UploadServiceDefinition
-    # arcpy.UploadServiceDefinition_server(sd, connectionfile)
-else:
-    # if the sddraft analysis contained errors, display them
-    print(analysis["errors"])
+    summary = "Survey Protocol " + PROTOCOL
+    tags = "Survey, Protocol, Park Observer, " + PROTOCOL
+
+    # create service definition draft
+    analysis = arcpy.mapping.CreateMapSDDraft(
+        map_doc,
+        sddraft,
+        PROTOCOL,
+        "ARCGIS_SERVER",
+        None,
+        False,
+        "ParkObserver",
+        summary,
+        tags,
+    )
+
+    # stage and upload the service if the sddraft analysis did not contain errors
+    if analysis["errors"] == {}:
+        # Execute StageService
+        arcpy.StageService_server(sddraft, sd_file)
+        # Execute UploadServiceDefinition
+        # arcpy.UploadServiceDefinition_server(sd_file, connectionfile)
+    else:
+        # if the sddraft analysis contained errors, display them
+        print(analysis["errors"])
+
+if __name__ == "__main__":
+    main()
